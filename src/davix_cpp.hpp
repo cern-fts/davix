1bdb17508b9e8fb849118babc325d955baed4173
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-05-25 09:31:22 +0000

    - add missing header file
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@6215 4525493e-7705-40b1-a816-d608a930855b

---
+++ src/davix_cpp.hpp
@@ -0,0 +1,20 @@
+#ifndef DAVIX_CPP_HPP
+#define DAVIX_CPP_HPP
+
+#include <composition.h>
+
+namespace Davix{
+
+/**
+    Create a Davix session, the main entry point of the Davix Library
+
+*/
+Composition* session_create();
+
+
+
+
+}
+
+
+#endif // DAVIX_CPP_HPP

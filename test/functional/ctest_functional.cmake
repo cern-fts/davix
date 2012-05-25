82f9c7ac7a043cc4a1b2b31238146fd7c0e432ed
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-05-25 09:32:22 +0000

    -first validation of mkdir
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@6227 4525493e-7705-40b1-a816-d608a930855b

--- test/functional/ctest_functional.cmake
+++ test/functional/ctest_functional.cmake
@@ -4,5 +4,8 @@
 message (" Setup tests parameters... ")
 
 
-test_dav_endpoint( "http://sligo.desy.de:2880/" "")
-test_dav_endpoint( "https://lxfsra04a04.cern.ch/dpm/" "${CMAKE_SOURCE_DIR}/test.p12")
+test_dav_endpoint_ronly( "http://sligo.desy.de:2880/pnfs/desy.de/data/dteam/" "")
+test_dav_endpoint_rw( "https://lxfsra04a04.cern.ch/dpm/cern.ch/home/dteam" "${CMAKE_SOURCE_DIR}/test.p12")
+
+# localhost generic server, ex : "davserver -n -D /tmp"
+test_dav_endpoint_rw("http://localhost:8008" " ")
---
+++ test/functional/test_mkdir.cpp
@@ -0,0 +1,66 @@
+#include "test_stat.h"
+
+#include <core.h>
+#include <http_backend.h>
+#include <glibmm/init.h>
+
+#include <string>
+#include <sstream>
+#include <cmath>
+
+using namespace Davix;
+
+
+int mycred_auth_callback(davix_auth_t token, const davix_auth_info_t* t, void* userdata, GError** err){
+    GError * tmp_err=NULL;
+    int ret = davix_set_pkcs12_auth(token, (const char*)userdata, (const char*)NULL, &tmp_err);
+    if(ret != 0){
+        fprintf(stderr, " FATAL authentification Error : %s", tmp_err->message);
+        g_propagate_error(err, tmp_err);
+    }
+
+    return ret;
+}
+
+
+static void configure_grid_env(char * cert_path, const Glib::RefPtr<Core>  & core){
+    AbstractSessionFactory* f = core->getSessionFactory();
+    f->set_ssl_ca_check(false);            // disable ssl ca check
+    f->set_authentification_controller(cert_path, &mycred_auth_callback);
+}
+
+int main(int argc, char** argv){
+    if( argc < 2){
+        std::cout << "Usage  : " << std::endl;
+        std::cout <<"\t" << argv[0] << " [url]" << std::endl;
+        std::cout <<"\t" << argv[0] << " [url] [CERTIFICATE_PATH] " << std::endl;
+        return 0;
+    }
+
+    srand(time(NULL));
+    g_logger_set_globalfilter(G_LOG_LEVEL_MASK);
+
+    try{
+        Glib::RefPtr<Core> c= Core::create(new NEONSessionFactory());
+        if(argc > 2){
+            configure_grid_env(argv[2], c);
+        }
+
+        std::ostringstream oss;
+        oss << argv[1] << "/"<< (rand()%20000);
+        std::string a = oss.str();
+
+        c->mkdir(a.c_str(), 0777);
+
+        std::cout << "mkdir  success !" << std::endl;
+
+    }catch(Glib::Error & e){
+        std::cout << " error occures : N°" << e.code() << "  " << e.what() << std::endl;
+        return -1;
+    }catch(std::exception & e){
+        std::cout << " error occures :" << e.what() << std::endl;
+        return -1;
+    }
+    return 0;
+}
+

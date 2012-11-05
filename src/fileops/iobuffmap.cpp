c4bb115aa858a6a548c762a272737cdedf0cef0e
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-11-05 10:14:55 +0000

    -add struct of IOMapper
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@7643 4525493e-7705-40b1-a816-d608a930855b

--- src/fileops/iobuffmap.cpp
+++ src/fileops/iobuffmap.cpp
@@ -6,4 +6,28 @@ IOBuffMap::IOBuffMap(Context & c, const Uri & uri, const RequestParams & params)
 {
 }
 
+bool IOBuffMap::open(int flags, DavixError **err){
+    return false;
+}
+
+ssize_t IOBuffMap::read(void *buf, size_t count, DavixError **err){
+    return -1;
+}
+
+ssize_t IOBuffMap::write(const void *buf, size_t count, DavixError **err){
+    return -1;
+}
+
+off_t IOBuffMap::lseek(off_t offset, int flags, DavixError **err){
+    return -1;
+}
+
+ssize_t IOBuffMap::putOps(const void *buf, size_t count, off_t offset, DavixError **err){
+    return -1;
+}
+
+ssize_t IOBuffMap::getOps(void *buf, size_t count, off_t offset, DavixError **err){
+    return -1;
+}
+
 } // namespace Davix
--- src/fileops/iobuffmap.hpp
+++ src/fileops/iobuffmap.hpp
@@ -5,6 +5,7 @@
 #include <davixcontext.hpp>
 #include <davixuri.hpp>
 #include <params/davixrequestparams.hpp>
+#include <libs/lockers/dpplocker.hpp>
 
 
 namespace Davix {
@@ -18,10 +19,36 @@ class IOBuffMap
 public:
     IOBuffMap(Context & c, const Uri & uri, const RequestParams & params);
 
+    // open the file associated with the davix IOBuffMap
+    // do a simple check if the file exist and try to anticipate the next ops
+    bool open(int flags, DavixError** err);
+
+    //
+    ssize_t read(void* buf, size_t count, DavixError** err);
+
+    //
+    ssize_t write(const void* buf, size_t count, DavixError** err);
+
+
+    //
+    off_t lseek(off_t offset, int flags, DavixError** err);
+
+    // position independant read operation,
+    // similar to pread except that does not need open() before
+    ssize_t getOps(void* buf, size_t count, off_t offset, DavixError** err);
+
+    // position independant write operation,
+    // similar to pwrite do not need open() before
+    ssize_t putOps(const void* buf, size_t count, off_t offset, DavixError** err);
+
 private:
     Context & _c;
     Uri _uri;
     RequestParams _params;
+    off_t pos;
+    HttpRequest * req;
+    DppLock rwlock;
+
 };
 
 } // namespace Davix

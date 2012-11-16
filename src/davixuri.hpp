c3b70d471037a0780d5f24377dbc163468167de6
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-11-16 09:12:30 +0000

    improve documentation of Davix URI
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@7809 4525493e-7705-40b1-a816-d608a930855b

--- src/davixuri.hpp
+++ src/davixuri.hpp
@@ -13,36 +13,55 @@
   @brief C++ URI utilities functions for davix
  */
 
-
 namespace Davix {
 
 struct UriPrivate;
 
-// convenience class for uri parsing
+/// @class Uri
+/// convenience class for uri parsing
+///
 class Uri
 {
 public:
+    /// Construct an empty invalid Uri
     Uri();
+    /// construct a new Davix Uri from a string URL
     Uri(const std::string & uri_string);
+    /// Copy constructor
     Uri(const Uri & uri);
     virtual ~Uri();
 
-    /// get a string representation of the uri
+    /// get a string representation of the full uri
+    /// @return return the path or an empty string if error
     const std::string & getString() const;
 
-    /// return the port number
+    /// get the port number
+    /// @return return the  port number of 0 if error
     int getPort() const;
 
+    /// get the protocol scheme
+    ///  @return return the protocol scheme or an empty string if error
     const std::string & getProtocol() const;
 
+    /// get the host name
+    /// @return return the hostname or an empty string if error
     const std::string & getHost() const;
 
+    /// get the path part of the Uri
+    /// @return return the path of the Uri or an empty string if error
     const std::string & getPath()const;
 
+    /// get a concatenation of the path and the query argument of the URI
+    /// @return return a path + query arguments concatenation or an empty string if error
     const std::string & getPathAndQuery() const;
 
+    /// get the query argument part of the uri
+    /// @return return the query path string or an empty string if not exist or if error
     const std::string & getQuery() const;
 
+    /// Status of the Uri
+    /// see \ref Davix::StatusCode
+    /// @return StatusCode::OK if success or StatusCode::UriParsingError if error
     StatusCode::Code getStatus() const;
 
     Uri & operator=(const Uri & orig);

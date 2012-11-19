beab9250d9dab6d429778b963b000f9bfde71840
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-11-19 14:43:43 +0000

    - add script for setup test env
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@7839 4525493e-7705-40b1-a816-d608a930855b

---
+++ test/setup_test_env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+MY_DIR=`dirname $0`
+
+ARGS="$1"
+
+export ACTION="start"
+
+if [[ "$1" == "stop" ]];
+then
+	ACTION="stop"
+fi
+
+
+
+OPTS_SERV_1=" -D /tmp -n -d ${ACTION} -P 8008 -i 0"
+OPTS_SERV_2=" -D /tmp -u test -p tester -d ${ACTION}  -P 8009 -i 1"
+
+export PYTHONPATH=$MY_DIR/
+
+
+$MY_DIR/pywebdav/server/server.py $OPTS_SERV_1
+
+
+$MY_DIR/pywebdav/server/server.py $OPTS_SERV_2
+
+
+

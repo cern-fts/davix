7eabf118024d110ca7613570df620385dedc7d0b
adevress <adevress@4525493e-7705-40b1-a816-d608a930855b>
2012-12-19 10:44:17 +0000

    - add minimal support for credential load
    git-svn-id: https://svn.cern.ch/reps/lcgdm/davix/trunk@8080 4525493e-7705-40b1-a816-d608a930855b

--- src/auth/x509cred.cpp
+++ src/auth/x509cred.cpp
@@ -2,8 +2,17 @@
 
 namespace Davix {
 
-X509Credential::X509Credential()
+struct X509CredentialInternal{
+    int a;
+};
+
+X509Credential::X509Credential() :
+    d_ptr(new X509CredentialInternal())
 {
+}
+
+X509Credential::~X509Credential(){
+    delete d_ptr;
 }
 
 } // namespace DAvix
--- src/auth/x509cred.hpp
+++ src/auth/x509cred.hpp
@@ -6,10 +6,16 @@
 
 namespace Davix {
 
+struct X509CredentialInternal;
+
 class X509Credential
 {
 public:
     X509Credential();
+    ~X509Credential();
+
+private:
+    X509CredentialInternal * d_ptr;
 };
 
 } // namespace DAvix

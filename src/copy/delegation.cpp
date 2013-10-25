fb6662ef2fb9e24c22dc1200dd7440cbce663e2a
Alejandro Alvarez Ayllon <alejandro.alvarez.ayllon@cern.ch>
2013-10-25 14:34:28 +0200

    On 3rd party copy, use shorter delegation lifetime
    Before it didn't matter, but with new GridSite based on canl,
    giving a longer lifetime doesn't work.

--- src/copy/delegation.cpp
+++ src/copy/delegation.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <gridsite.h>
 #include <status/davixstatusrequest.hpp>
 #include <stdsoap2.h>
@@ -10,6 +11,55 @@
 #include "copy_internal.hpp"
 
 using namespace Davix;
+
+int get_timestamp_from_asn1(ASN1_TIME* asn1)
+{
+    char* data = (char*) ASN1_STRING_data(asn1);
+    size_t len = strlen(data);
+    struct tm time_tm;
+    char zone = 0;
+
+    memset(&time_tm, 0, sizeof(struct tm));
+
+    if (len == 13
+            && sscanf(data, "%02d%02d%02d%02d%02d%02d%c", &(time_tm.tm_year),
+                    &(time_tm.tm_mon), &(time_tm.tm_mday), &(time_tm.tm_hour),
+                    &(time_tm.tm_min), &(time_tm.tm_sec), &zone) != 7) {
+        return 0;
+    }
+    else if (len == 15
+            && sscanf(data, "20%02d%02d%02d%02d%02d%02d%c", &(time_tm.tm_year),
+                    &(time_tm.tm_mon), &(time_tm.tm_mday), &(time_tm.tm_hour),
+                    &(time_tm.tm_min), &(time_tm.tm_sec), &zone) != 7) {
+        return 0;
+    }
+
+    if (zone != 'Z')
+        return 0;
+
+    if (time_tm.tm_year < 90)
+        time_tm.tm_year += 100;
+    --(time_tm.tm_mon);
+
+    return timegm(&time_tm);
+}
+
+int get_cert_remaining_life(const std::string& cert)
+{
+    FILE* f = fopen(cert.c_str(), "r");
+    if (f == NULL)
+        return 0;
+    
+    X509 *x509_cert = PEM_read_X509(f, NULL, NULL, NULL);
+    if (x509_cert == NULL)
+        return 0;
+    
+    ASN1_TIME* expiration = X509_get_notAfter(x509_cert);
+    int expiration_timestamp = get_timestamp_from_asn1(expiration);
+    X509_free(x509_cert);
+    
+    return (expiration_timestamp - time(NULL)) / 60;
+}
 
 /// Do the delegation
 std::string davix_delegate(const std::string &urlpp,
@@ -55,8 +105,19 @@ std::string davix_delegate(const std::string &urlpp,
   if (capathList.size() > 0)
       capath = capathList[0];
 
-  // TODO: Get from the environment or something
-  lifetime = 12 * 60 * 60; // 12h
+  // Delegation lifetime (in minutes!)
+  int cert_remaining_life = get_cert_remaining_life(ucert);
+  int delegation_max_life = 12 * 60; // 12 hours
+  
+  // Delegated proxy lifetime should be shorter than the current lifetime!
+  lifetime = std::min(cert_remaining_life, delegation_max_life) - 1;
+  
+  // Should at least remain one minute
+  if (lifetime <= 1) {
+      DavixError::setupError(err, COPY_SCOPE, DAVIX_STATUS_DELEGATION_ERROR,
+                             std::string("The certificate expired or has less than two minutes left!"));
+      return "";
+  }
 
   // Cert and key need to be in the same file
   if (ucert == ukey) {

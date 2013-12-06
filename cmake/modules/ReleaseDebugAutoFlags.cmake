bb79f9160609c0a5559a2a78b6c90d36d8c98938
Adrien Devresse <adevress@cern.ch>
2013-12-06 19:53:58 +0100

    - correct default compile flags

--- cmake/modules/ReleaseDebugAutoFlags.cmake
+++ cmake/modules/ReleaseDebugAutoFlags.cmake
@@ -8,5 +8,5 @@ set(CMAKE_C_FLAGS_RELWITHDEBINFO " -Wall -g -O2")
 
 
 set(CMAKE_CXX_FLAGS_RELEASE  "-O2")
-set(CMAKE_CXX_FLAGS_DEBUG  "-g -Wall -Weffc++ -fstack-protector-all ")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO " -Wall -Weffc++ -g -O2")
+set(CMAKE_CXX_FLAGS_DEBUG  "-g -Wall -fstack-protector-all ")
+set(CMAKE_CXX_FLAGS_RELWITHDEBINFO " -Wall -g -O2")

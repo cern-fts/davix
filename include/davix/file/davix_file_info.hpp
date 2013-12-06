5f65eb19b09fbab3dddea9af621a50c89d58876f
Adrien Devresse <adevress@cern.ch>
2013-12-06 18:51:55 +0100

    - switch files info to copyable status

--- include/davix/file/davix_file_info.hpp
+++ include/davix/file/davix_file_info.hpp
@@ -19,11 +19,13 @@ namespace Davix{
 ///
 ///  Base struct for any generic
 ///
-class DAVIX_EXPORT FileInfoInterface : NonCopyable{
+class DAVIX_EXPORT FileInfoInterface{
 public:
     virtual ~FileInfoInterface(){}
 
     virtual const std::type_info & getType() =0;
+
+    virtual FileInfoInterface* getClone() = 0;
 };
 
 
@@ -34,6 +36,10 @@ public:
 
     virtual const std::type_info & getType(){
         return typeid(*static_cast<T*>(this));
+    }
+
+    virtual FileInfoInterface* getClone(){
+        return new T(*static_cast<T*>(this));
     }
 };
 
@@ -47,6 +53,7 @@ class DAVIX_EXPORT FileInfoSize :
 public:
     FileInfoSize() : size(0){}
     FileInfoSize(dav_size_t s) : size(s){}
+    FileInfoSize(const FileInfoSize & orig) : size(orig.size) {}
     virtual ~FileInfoSize(){}
     dav_size_t size;
 };
@@ -58,6 +65,7 @@ public:
 class DAVIX_EXPORT FileInfoHash :
     public FileInfo<FileInfoHash>{
 public:
+    FileInfoHash(const FileInfoHash & orig) : hash(orig.hash), hash_type(orig.hash_type) {}
     virtual ~FileInfoHash(){}
     std::string hash;
     std::string hash_type;
@@ -70,6 +78,7 @@ public:
 class DAVIX_EXPORT FileInfoName :
     public FileInfo<FileInfoName>{
 public:
+    FileInfoName(const FileInfoName & orig) : name(orig.name){}
     virtual ~FileInfoName(){}
     std::string name;
 };
@@ -83,6 +92,7 @@ class DAVIX_EXPORT FileInfoProtocolType :
     public FileInfo<FileInfoProtocolType>{
 public:
     FileInfoProtocolType() : protocol(){}
+    FileInfoProtocolType(const FileInfoProtocolType & orig) : protocol(orig.protocol){}
     FileInfoProtocolType(const std::string & s) : protocol(s){}
     virtual ~FileInfoProtocolType(){}
     std::string protocol;
@@ -103,9 +113,17 @@ typedef std::vector<FileInfoInterface*> Properties;
 /// @brief Replica of a resource
 class DAVIX_EXPORT Replica: NonCopyable{
 public:
-    inline Replica() :
+    Replica() :
         uri(),
         props(){}
+    Replica(const Replica & orig) :
+        uri(orig.uri),
+        props() {
+        props.reserve(orig.props.size());
+        for(Properties::iterator it = props.begin(); it < props.end(); ++it){
+            props.push_back((*it)->getClone());
+        }
+    }
 
     virtual ~Replica(){
         for(Properties::iterator it = props.begin(); it < props.end(); ++it)

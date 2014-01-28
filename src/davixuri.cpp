7968f6a8de70c0188fb650e31165df16ad183883
Adrien Devresse <adevress@cern.ch>
2014-01-28 23:50:37 +0100

    - improve uri Parser, decoupled from neon

--- src/davixuri.cpp
+++ src/davixuri.cpp
@@ -3,6 +3,7 @@
 #include <ne_uri.h>
 #include <cassert>
 #include <cstring>
+#include <cstdlib>
 
 namespace Davix {
 
@@ -10,71 +11,66 @@ namespace Davix {
  * @cond HIDDEN_SYMBOLS
  */
 
-static std::string void_str;
+const std::string void_str("");
+
+int davix_uri_parse(const std::string & uri_str, UriPrivate & res);
+int davix_uri_cmp(const UriPrivate &u1, const UriPrivate &u2);
+std::string davix_path_escape(const std::string & str);
+std::string davix_path_unescape(const std::string & str);
 
 struct UriPrivate{
     UriPrivate() :
-        my_uri(),
         code(StatusCode::UriParsingError),
         proto(),
+        userinfo(),
         path(),
         host(),
         query(),
-        query_and_path(),
-        _uri_string(){
-
-        memset(&my_uri, 0, sizeof(my_uri));
-    }
+        port(0),
+        _uri_string(),
+        query_and_path(NULL){}
 
     UriPrivate(const UriPrivate & orig):
-        my_uri(),
         code(orig.code),
         proto(orig.proto),
+        userinfo(orig.userinfo),
         path(orig.path),
         host(orig.host),
         query(orig.query),
-        query_and_path(orig.query_and_path),
-        _uri_string(orig._uri_string){
-        ne_uri_copy(&my_uri, &(orig.my_uri));
-
-    }
+        port(orig.port),
+        _uri_string(orig._uri_string),
+        query_and_path(NULL){ }
 
     ~UriPrivate() {
-        ne_uri_free(&my_uri);
+        delete query_and_path;
     }
 
     void parsing(const std::string & uri_string){
         _uri_string = uri_string;
-        if(ne_uri_parse(uri_string.c_str(), &(my_uri)) == 0){
-            if(my_uri.scheme == NULL
-               || my_uri.path == NULL
-               || my_uri.host ==NULL)
-                return;
-
-            if(my_uri.port == UINT_MAX)
-                return;
-
+        if( davix_uri_parse(_uri_string, *this) == 0
+             && proto.size() != 0
+             && port != UINT_MAX){
             code = StatusCode::OK;
-            proto = my_uri.scheme;
-            path = my_uri.path;
-            host = my_uri.host;
-            if(my_uri.query){
-                query = my_uri.query;
-                query_and_path= path + "?" + query;
-            }else{
-                query_and_path = path;
-            }
+        }else{
+            clear();
         }
     }
 
-    ne_uri my_uri;
+    void clear(){
+        proto.clear();
+        userinfo.clear();
+        path.clear();
+        host.clear();
+        query.clear();
+        fragment.clear();
+        port = 0;
+    }
+
     StatusCode::Code code;
-    std::string proto;
-    std::string path;
-    std::string host;
-    std::string query;
-    std::string query_and_path;
+    std::string proto, userinfo, path, host, query, fragment;
+    unsigned int port;
     std::string _uri_string;
+    std::string* query_and_path;
 
 };
 
@@ -93,19 +89,16 @@ Uri::Uri(const Uri & uri) :
 }
 
 Uri::~Uri(){
-    ne_uri_free(&(d_ptr->my_uri));
     delete d_ptr;
 }
 
 int Uri::getPort() const{
     if(d_ptr->code != StatusCode::OK)
         return -1;
-    return d_ptr->my_uri.port;
+    return static_cast<int>(d_ptr->port);
 }
 
 const std::string & Uri::getHost() const{
-    if(d_ptr->code != StatusCode::OK)
-        return void_str;
     return d_ptr->host;
 }
 
@@ -114,30 +107,32 @@ const std::string & Uri::getString() const{
 }
 
 const std::string & Uri::getProtocol() const {
-    if(d_ptr->code != StatusCode::OK)
-        return void_str;
     return d_ptr->proto;
 }
 
 const std::string & Uri::getPath() const {
-    if(d_ptr->code != StatusCode::OK)
-        return void_str;
     return d_ptr->path;
 }
 
 const std::string & Uri::getPathAndQuery() const {
-    if(d_ptr->code != StatusCode::OK)
-        return void_str;
-    return d_ptr->query_and_path;
+    if(d_ptr->query_and_path == NULL){
+        if(d_ptr->query.size() > 0){
+            d_ptr->query_and_path = new std::string(d_ptr->path + "?" + d_ptr->query);
+        }else{
+            d_ptr->query_and_path = new std::string(d_ptr->path);
+        }
+    }
+    return *(d_ptr->query_and_path);
 }
 
 const std::string & Uri::getQuery() const{
-    if(d_ptr->code != StatusCode::OK)
-        return void_str;
     return d_ptr->query;
 }
 
 Uri & Uri::operator =(const Uri & orig){
+    if(this == &orig)
+        return *this;
+
     if (d_ptr) delete d_ptr;
     d_ptr = new UriPrivate(*(orig.d_ptr));
     return *this;
@@ -147,10 +142,12 @@ StatusCode::Code Uri::getStatus() const{
     return d_ptr->code;
 }
 
+
 bool Uri::equal(const Uri &u2) const{
     if(this->getStatus() != Davix::StatusCode::OK || u2.getStatus() != Davix::StatusCode::OK)
         return false;
-    return ne_uri_cmp(&this->d_ptr->my_uri, &u2.d_ptr->my_uri) ==0;
+
+    return davix_uri_cmp(*d_ptr, *(u2.d_ptr))==0;
 }
 
 bool Uri::operator ==(const Uri & u2) const{
@@ -159,17 +156,11 @@ bool Uri::operator ==(const Uri & u2) const{
 
 
 std::string Uri::escapeString(const std::string & str){
-    char* p = ne_path_escape(str.c_str());
-    std::string  res(p);
-    free(p);
-    return res;
+    return davix_path_escape(str);
 }
 
 std::string Uri::unescapeString(const std::string & str){
-    char* p = ne_path_unescape(str.c_str());
-    std::string res(p);
-    free(p);
-    return res;
+    return davix_path_unescape(str);
 }
 
 bool uriCheckError(const Uri &uri, DavixError **err){
@@ -192,6 +183,293 @@ unsigned int httpUriGetPort(const Uri & uri){
     }
     return port;
 }
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+///   part imported from neon parser ne_uri,  converted C++
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+/* URI ABNF from RFC 3986: */
+
+#define PS (0x0001) /* "+" */
+#define PC (0x0002) /* "%" */
+#define DS (0x0004) /* "-" */
+#define DT (0x0008) /* "." */
+#define US (0x0010) /* "_" */
+#define TD (0x0020) /* "~" */
+#define FS (0x0040) /* "/" */
+#define CL (0x0080) /* ":" */
+#define AT (0x0100) /* "@" */
+#define QU (0x0200) /* "?" */
+
+#define DG (0x0400) /* DIGIT */
+#define AL (0x0800) /* ALPHA */
+
+#define GD (0x1000) /* gen-delims    = "#" / "[" / "]"
+                     * ... except ":", "/", "@", and "?" */
+
+#define SD (0x2000) /* sub-delims    = "!" / "$" / "&" / "'" / "(" / ")"
+                     *               / "*" / "+" / "," / ";" / "="
+                     * ... except "+" which is PS */
+
+#define OT (0x4000) /* others */
+
+#define URI_ALPHA (AL)
+#define URI_DIGIT (DG)
+
+/* unreserved = ALPHA / DIGIT / "-" / "." / "_" / "~" */
+#define URI_UNRESERVED (AL | DG | DS | DT | US | TD)
+/* scheme = ALPHA *( ALPHA / DIGIT / "+" / "-" / "." ) */
+#define URI_SCHEME (AL | DG | PS | DS | DT)
+/* real sub-delims definition, including "+" */
+#define URI_SUBDELIM (PS | SD)
+/* real gen-delims definition, including ":", "/", "@" and "?" */
+#define URI_GENDELIM (GD | CL | FS | AT | QU)
+/* userinfo = *( unreserved / pct-encoded / sub-delims / ":" ) */
+#define URI_USERINFO (URI_UNRESERVED | PC | URI_SUBDELIM | CL)
+/* pchar = unreserved / pct-encoded / sub-delims / ":" / "@" */
+#define URI_PCHAR (URI_UNRESERVED | PC | URI_SUBDELIM | CL | AT)
+/* invented: segchar = pchar / "/" */
+#define URI_SEGCHAR (URI_PCHAR | FS)
+/* query = *( pchar / "/" / "?" ) */
+#define URI_QUERY (URI_PCHAR | FS | QU)
+/* fragment == query */
+#define URI_FRAGMENT URI_QUERY
+
+/* any characters which should be path-escaped: */
+#define URI_ESCAPE ((URI_GENDELIM & ~(FS)) | URI_SUBDELIM | OT | PC)
+
+static const unsigned int uri_chars[256] = {
+/* 0xXX    x0      x2      x4      x6      x8      xA      xC      xE     */
+/*   0x */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   1x */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   2x */ OT, SD, OT, GD, SD, PC, SD, SD, SD, SD, SD, PS, SD, DS, DT, FS,
+/*   3x */ DG, DG, DG, DG, DG, DG, DG, DG, DG, DG, CL, SD, OT, SD, OT, QU,
+/*   4x */ AT, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL,
+/*   5x */ AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, GD, OT, GD, OT, US,
+/*   6x */ OT, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL,
+/*   7x */ AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, AL, OT, OT, OT, TD, OT,
+/*   8x */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   9x */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Ax */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Bx */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Cx */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Dx */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Ex */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT,
+/*   Fx */ OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT, OT
+};
+
+#define uri_lookup(ch) (uri_chars[(unsigned char)ch])
+
+
+int davix_uri_parse(const std::string & uri_str, UriPrivate & res)
+{
+    const char *p, *s, *uri;
+    p = s = uri = uri_str.c_str();
+
+    /* => s = p = URI-reference */
+
+    if (uri_lookup(*p) & URI_ALPHA) {
+        while (uri_lookup(*p) & URI_SCHEME)
+            p++;
+
+        if (*p == ':') {
+            res.proto.assign(uri, p -s);
+            s = p + 1;
+        }
+    }
+
+    /* => s = heir-part, or s = relative-part */
+
+    if (s[0] == '/' && s[1] == '/') {
+        const char *pa;
+
+        /* => s = "//" authority path-abempty (from expansion of
+         * either heir-part of relative-part)  */
+
+        /* authority = [ userinfo "@" ] host [ ":" port ] */
+
+        s = pa = s + 2; /* => s = authority */
+
+        while (*pa != '/' && *pa != '\0')
+            pa++;
+        /* => pa = path-abempty */
+
+        p = s;
+        while (p < pa && uri_lookup(*p) & URI_USERINFO)
+            p++;
+
+        if (*p == '@') {
+            res.userinfo.assign(s, p-s);
+            s = p + 1;
+        }
+        /* => s = host */
+
+        if (s[0] == '[') {
+            p = s + 1;
+
+            while (*p != ']' && p < pa)
+                p++;
+
+            if (p == pa || (p + 1 != pa && p[1] != ':')) {
+                /* Ill-formed IP-literal. */
+                return -1;
+            }
+
+            p++; /* => p = colon */
+        } else {
+            /* Find the colon. */
+            p = pa;
+            while (*p != ':' && p > s)
+                p--;
+        }
+
+        if (p == s) {
+            p = pa;
+            /* No colon; => p = path-abempty */
+        } else if (p + 1 != pa) {
+            /* => p = colon */
+            res.port = atoi(p + 1);
+            if( res.port  == 0)
+                 res.port  = UINT_MAX;
+        }
+        res.host.assign(s, p - s);
+
+        s = pa;
+
+        if (*s == '\0') {
+            s = "/"; /* FIXME: scheme-specific. */
+        }
+    }
+
+    /* => s = path-abempty / path-absolute / path-rootless
+     *      / path-empty / path-noscheme */
+
+    p = s;
+
+    while (uri_lookup(*p) & URI_SEGCHAR)
+        p++;
+
+    /* => p = [ "?" query ] [ "#" fragment ] */
+
+    res.path.assign(s, p - s);
+
+    if (*p != '\0') {
+        s = p++;
+
+        while (uri_lookup(*p) & URI_QUERY)
+            p++;
+
+        /* => p = [ "#" fragment ] */
+        /* => s = [ "?" query ] [ "#" fragment ] */
+
+        if (*s == '?') {
+            res.query.assign(s + 1, p - s - 1);
+
+            if (*p != '\0') {
+                s = p++;
+
+                while (uri_lookup(*p) & URI_FRAGMENT)
+                    p++;
+            }
+        }
+
+        /* => p now points to the next character after the
+         * URI-reference; which should be the NUL byte. */
+
+        if (*s == '#') {
+            res.fragment.assign(s + 1, p - s - 1);
+        }
+        else if (*p || *s != '?') {
+            return -1;
+        }
+    }
+
+    return 0;
+}
+
+
+// As specified by RFC 2616, section 3.2.3.
+int davix_uri_cmp(const UriPrivate & u1, const UriPrivate & u2)
+{
+
+    int diff;
+    if( (diff= u1.path.compare(u2.path)) != 0)
+        return diff;
+    if( (diff= strcasecmp(u1.host.c_str(), u2.host.c_str())) != 0)
+        return diff;
+    if( (diff= strcasecmp(u1.proto.c_str(), u2.proto.c_str())) != 0)
+        return diff;
+    if( (diff= u1.query.compare(u2.query)) != 0)
+        return diff;
+    if( (diff= u1.fragment.compare(u2.fragment)) != 0)
+        return diff;
+    if( (diff= u1.userinfo.compare(u2.userinfo)) != 0)
+        return diff;
+    return u2.port - u1.port;
+}
+
+
+
+std::string davix_path_unescape(const std::string & str)
+{
+    const char *pnt, *uri = (const  char*)str.c_str();
+    char *retpos, buf[5] = { "0x00" };
+
+    char buffer[str.size() + 1];
+    retpos = buffer;
+    for (pnt = uri; *pnt != '\0'; pnt++) {
+    if (*pnt == '%') {
+        if (!isxdigit((unsigned char) pnt[1]) ||
+        !isxdigit((unsigned char) pnt[2])) {
+        /* Invalid URI */
+                return std::string();
+        }
+        buf[2] = *++pnt; buf[3] = *++pnt; /* bit faster than memcpy */
+        *retpos++ = (char)strtol(buf, NULL, 16);
+    } else {
+        *retpos++ = *pnt;
+    }
+    }
+    *retpos = '\0';
+    return std::string(buffer);
+}
+
+/* CH must be an unsigned char; evaluates to 1 if CH should be
+ * percent-encoded. */
+#define path_escape_ch(ch) (uri_lookup(ch) & URI_ESCAPE)
+
+std::string davix_path_escape(const std::string & str)
+{
+    const unsigned char *pnt, *path= (const unsigned char*)(str.c_str());
+    char *p;
+    size_t count = 0;
+
+    for (pnt = (const unsigned char *)path; *pnt != '\0'; pnt++) {
+        count += path_escape_ch(*pnt);
+    }
+
+    if (count == 0) {
+        return std::string((const char*) path);
+    }
+
+    char buffer[str.size() + 2 * count + 1];
+    p = buffer;
+    for (pnt = (const unsigned char *)path; *pnt != '\0'; pnt++) {
+    if (path_escape_ch(*pnt)) {
+        /* Escape it - %<hex><hex> */
+        sprintf(p, "%%%02x", (unsigned char) *pnt);
+        p += 3;
+    } else {
+        *p++ = *pnt;
+    }
+    }
+    *p = '\0';
+    return std::string(buffer);
+}
+
+#undef path_escape_ch
 
 
 } // namespace Davix
--- test/unit/neon/test_neon.cpp
+++ test/unit/neon/test_neon.cpp
@@ -1,6 +1,8 @@
 #include <davix.hpp>
 #include <string>
+#include <sstream>
 #include <cstring>
+#include <algorithm>
 #include <gtest/gtest.h>
 
 using namespace Davix;
@@ -47,7 +49,7 @@ static bool failure[]={
     true
 };
 
-const size_t len_list=4;
+const size_t len_list= (sizeof(list_proto))/(sizeof(const char*));
 
 
 TEST(testNeon, testParsing){
@@ -67,3 +69,32 @@ TEST(testNeon, testParsing){
 }
 
 
+TEST(testUri, testCopyEQ){
+    Uri uri;
+    std::ostringstream ss;
+    ASSERT_EQ(uri.getStatus(), StatusCode::UriParsingError);
+    uri = Uri("http://wikipedia.org:80/bonus?salut");
+    ASSERT_EQ(uri.getStatus(), StatusCode::OK);
+
+    ss << uri;
+    ASSERT_STREQ(ss.str().c_str(),uri.getString().c_str());
+
+    std::string pathAndString("http://wikipedia.org:80/bonus?salut=bob");
+    uri = pathAndString;
+    std::string::reverse_iterator rit = std::find(pathAndString.rbegin(), pathAndString.rend(), '/');
+    std::string::iterator it = pathAndString.begin() + (pathAndString.rend() - rit);
+    ASSERT_STREQ(uri.getPathAndQuery().c_str(), std::string(it-1, pathAndString.end()).c_str() );
+
+    Uri uri2(uri);
+    ASSERT_TRUE(uri == uri2);
+}
+
+
+
+TEST(testUri, testEscape){
+
+    std::string s("http://wikipedia.org:80/bonus a toi \\ é voilù ?salut");
+
+    std::string r = Davix::Uri::unescapeString(Davix::Uri::escapeString(s));
+    ASSERT_STREQ(s.c_str(), r.c_str());
+}

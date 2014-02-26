a9eaa69014da9fb04a8e7b67b88c248d6fb12978
Adrien Devresse <adevress@cern.ch>
2014-02-27 00:19:10 +0100

    Include license file header LGPLv2.1, include davix_internal header

--- deps/simple_getpass/simple_get_pass.h
+++ deps/simple_getpass/simple_get_pass.h
@@ -1,6 +1,28 @@
+/*
+ * This File is part of Davix, The IO library for HTTP based protocols
+ * Copyright (C) 2013  Adrien Devresse <adrien.devresse@cern.ch>, CERN
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+*/
+
 #pragma once
 #ifndef SIMPLE_GET_PASS_H
 #define SIMPLE_GET_PASS_H
+
+
 
 
 #ifdef __cplusplus

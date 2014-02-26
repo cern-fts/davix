847505ce72ef7972e7852ce3d26fccdfa597d7c6
Adrien Devresse <adevress@cern.ch>
2014-02-27 00:19:10 +0100

    Include license file header LGPLv2.1, include davix_internal header

--- src/tools/davix_tool_params.hpp
+++ src/tools/davix_tool_params.hpp
@@ -1,3 +1,24 @@
+/*
+ * This File is part of Davix, The IO library for HTTP based protocols
+ * Copyright (C) 2013  Adrien Devresse <adrien.devresse@cern.ch>, CERN
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+*/
+
+
 #ifndef DAVIX_TOOL_PARAMS_HPP
 #define DAVIX_TOOL_PARAMS_HPP
 

a57512cb0bc6d93a317e5aaa7bfbc70fae7bc6f9
Adrien Devresse <adevress@cern.ch>
2014-05-26 15:47:46 +0200

    - add Storm to the list of automated tested endpoints

--- test/functional/ctest_functional.cmake
+++ test/functional/ctest_functional.cmake
@@ -8,6 +8,7 @@ set(BASIC_PASSWD "tester")
 
 set(http_desy_base "https://vm-dcache-deploy6.desy.de:2880/dteam/davix-tests" CACHE STRING "dCache test instance to use")
 set(http_lcgdm_base "https://lxfsra04a04.cern.ch/dpm/cern.ch/home/dteam" CACHE STRING "DPM test instance to use" )
+set(http_storm_base "https://gridhttps-storm-atlas.cr.cnaf.infn.it:8443/webdav/dteam/davix-tests/" CACHE STRING "Storm test instance to use")
 set(http_dynaFed_base "http://dav-federation.cern.ch/myfed/dteam/")
 set(http_lcgdm_file "${http_lcgdm_base}/davix-tests/testread0011")
 set(http_dynaFed_file "${http_dynaFed_base}/davix-tests/testread0011")
@@ -17,11 +18,19 @@ set(metalink3_url "http://download.documentfoundation.org/libreoffice/stable/4.1
 set(metalink_url_direct "http://download.documentfoundation.org/libreoffice/stable/4.1.6/deb/x86_64/LibreOffice_4.1.6_Linux_x86-64_deb.tar.gz.meta4")
 
 test_dav_endpoint_ronly( ${http_desy_base} "proxy")
+test_valid_write_read_generic("${http_desy_base}" "proxy")
 
+# DPM tests
 test_dav_endpoint_rw( "${http_lcgdm_base}" "proxy")
-
 test_valid_delete_all("${http_lcgdm_base}"  "proxy")
 test_valid_read_generic("${http_lcgdm_base}" "proxy")
+test_valid_write_read_generic("${http_lcgdm_base}" "proxy")
+
+# Storm tests
+test_dav_endpoint_rw( "${http_storm_base}" "proxy")
+test_valid_delete_all("${http_storm_base}"  "proxy")
+test_valid_read_generic("${http_storm_base}" "proxy")
+test_valid_write_read_generic("${http_storm_base}" "proxy")
 
 # localhost generic server, ex : "davserver -n -D /tmp"
 test_dav_endpoint_rw("http://localhost:8008" "")
@@ -49,9 +58,6 @@ test_valid_read_generic("${http_desy_base}" "proxy")
 test_valid_read_generic("${http_dynaFed_base}" "proxy")
 test_valid_delete_all("${http_desy_base}"  "proxy")
 
-
-test_valid_write_read_generic("${http_lcgdm_base}" "proxy")
-test_valid_write_read_generic("${http_desy_base}" "proxy")
 
 # test replicas listing
 test_replica_listing_existing("${http_lcgdm_file}" "proxy")

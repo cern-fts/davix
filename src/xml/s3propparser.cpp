49c5e8cdb824120bd051b8a084578fd10f8e238f
Adrien Devresse <adevress@cern.ch>
2014-10-03 17:39:09 +0200

    Initial s3 parser sckeleton

---
+++ src/xml/s3propparser.cpp
@@ -0,0 +1,11 @@
+#include "s3propparser.hpp"
+
+namespace Davix{
+
+S3PropParser::S3PropParser()
+{
+}
+
+
+
+}
---
+++ src/xml/s3propparser.hpp
@@ -0,0 +1,34 @@
+#ifndef S3PROPPARSER_HPP
+#define S3PROPPARSER_HPP
+
+#include <deque>
+#include <xml/davxmlparser.hpp>
+#include <utils/davix_fileproperties.hpp>
+#include <string.h>
+
+namespace Davix{
+
+class S3PropParser : public XMLSAXParser
+{
+public:
+    S3PropParser();
+
+    struct Internal;
+    virtual ~S3PropParser();
+
+    std::deque<FileProperties> & getProperties();
+
+
+protected:
+    virtual int parserStartElemCb(int parent, const char *nspace, const char *name, const char **atts);
+    virtual int parserCdataCb(int state, const char *cdata, size_t len);
+    virtual int parserEndElemCb(int state, const char *nspace, const char *name);
+
+
+private:
+    Internal* d_ptr;
+};
+
+}
+
+#endif // S3PROPPARSER_HPP

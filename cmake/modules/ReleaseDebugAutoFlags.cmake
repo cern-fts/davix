e4503d4f23cf51d8db2de2a0aba741b881e67567
Georgios Bitzes <georgios.bitzes@cern.ch>
2016-01-12 11:13:11 +0100

    DMC-792 Add MSVC detection in cmake

--- cmake/modules/ReleaseDebugAutoFlags.cmake
+++ cmake/modules/ReleaseDebugAutoFlags.cmake
@@ -1,12 +1,20 @@
 ## debug / release autoManagement
 
 
+if(MSVC)
+    set(CMAKE_C_FLAGS_RELEASE  "-O2")
+    set(CMAKE_C_FLAGS_DEBUG  "-Wall")
+    set(CMAKE_C_FLAGS_RELWITHDEBINFO "-O2 -Wall")
 
-set(CMAKE_C_FLAGS_RELEASE  "-O2")
-set(CMAKE_C_FLAGS_DEBUG  "-g -Wall -Wextra -pedantic -fstack-protector-all")
-set(CMAKE_C_FLAGS_RELWITHDEBINFO " -Wall -g -O2")
+    set(CMAKE_CXX_FLAGS_RELEASE  "-O2")
+    set(CMAKE_CXX_FLAGS_DEBUG  "-Wall")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO "-O2 -Wall")
+else()
+    set(CMAKE_C_FLAGS_RELEASE  "-O2")
+    set(CMAKE_C_FLAGS_DEBUG  "-g -Wall -Wextra -pedantic -fstack-protector-all")
+    set(CMAKE_C_FLAGS_RELWITHDEBINFO " -Wall -g -O2")
 
-
-set(CMAKE_CXX_FLAGS_RELEASE  "-O2")
-set(CMAKE_CXX_FLAGS_DEBUG  "-g -Wall -Wextra  -fstack-protector-all ")
-set(CMAKE_CXX_FLAGS_RELWITHDEBINFO " -Wall -g -O2")
+    set(CMAKE_CXX_FLAGS_RELEASE  "-O2")
+    set(CMAKE_CXX_FLAGS_DEBUG  "-g -Wall -Wextra  -fstack-protector-all ")
+    set(CMAKE_CXX_FLAGS_RELWITHDEBINFO " -Wall -g -O2")
+endif()

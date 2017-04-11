41d8abcae82ce3fc8de2d24c59be29cb94fb9d34
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-04-11 11:41:22 +0200

    Also fix the tests

--- test/run-in-mock.sh
+++ test/run-in-mock.sh
@@ -53,7 +53,7 @@ else
 fi
 
 # Install deps
-mock_cmd install cmake git yum-utils abi-compliance-checker boost-devel doxygen gsoap-devel gtest-devel libxml2-devel openssl-devel voms-clients
+mock_cmd install cmake git yum-utils abi-compliance-checker boost-devel doxygen gsoap-devel gtest-devel libxml2-devel openssl-devel voms-clients libuuid-devel
 MOCK_HOME="/builddir"
 
 # copy grid-security and vomses

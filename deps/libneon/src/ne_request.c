9c2e5e32fd6c8e31c6a7a1551ece8e0954390318
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-09-17 14:07:19 +0200

    DMC-985 Fix libneon bug, wrong flag in out-of-bounds check

--- deps/libneon/src/ne_request.c
+++ deps/libneon/src/ne_request.c
@@ -535,7 +535,7 @@ void ne_set_request_body_fd(ne_request *req, int fd,
 
 void ne_set_request_flag(ne_request *req, ne_request_flag flag, int value)
 {
-    if (flag < NE_SESSFLAG_LAST) {
+    if (flag < NE_REQFLAG_LAST) {
         req->flags[flag] = value;
     }
 }

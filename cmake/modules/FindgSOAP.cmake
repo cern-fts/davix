a5f6a4394b21de0d11e9e9b2dee6aafc1aea4832
Georgios Bitzes <georgios.bitzes@cern.ch>
2017-10-30 13:40:32 +0100

    Prevent cmake invocation from hanging on Ubuntu 17.10
    Looks like soapcpp2 no longer includes version number
    when passing -help, and hangs when passing "-v",
    which is the fallback for Mac OS.. argh.

--- cmake/modules/FindgSOAP.cmake
+++ cmake/modules/FindgSOAP.cmake
@@ -3,7 +3,7 @@
 # include files and libraries are.
 #
 # This code sets the following variables:
-# 
+#
 # GSOAP_LIBRARIES       = full path to the gsoap libraries
 # GSOAP_SSL_LIBRARIES   = full path to the gsoap ssl libraries
 # GSOAP_INCLUDE_DIR     = include dir to be used when using the gsoap library
@@ -20,13 +20,13 @@
 # -----------------------------------------------------
 find_library(GSOAP_LIBRARIES
     NAMES gsoap
-    HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64 
+    HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64
           ${GSOAP_LOCATION}/lib32
     DOC "The main gsoap library"
 )
 find_library(GSOAP_SSL_LIBRARIES
     NAMES gsoapssl
-    HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64 
+    HINTS ${GSOAP_LOCATION}/lib ${GSOAP_LOCATION}/lib64
           ${GSOAP_LOCATION}/lib32
     DOC "The ssl gsoap library"
 )
@@ -34,7 +34,7 @@ find_library(GSOAP_SSL_LIBRARIES
 # -----------------------------------------------------
 # GSOAP Include Directories
 # -----------------------------------------------------
-find_path(GSOAP_INCLUDE_DIR 
+find_path(GSOAP_INCLUDE_DIR
     NAMES stdsoap2.h
     HINTS ${GSOAP_LOCATION} ${GSOAP_LOCATION}/include ${GSOAP_LOCATION}/include/*
     DOC "The gsoap include directory"
@@ -65,26 +65,26 @@ execute_process(COMMAND ${GSOAP_SOAPCPP2}  "-help"   OUTPUT_VARIABLE GSOAP_STRIN
 string(REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*" GSOAP_VERSION ${GSOAP_STRING_VERSION})
 
 if( "${GSOAP_VERSION}" STREQUAL "..")
-  execute_process(COMMAND ${GSOAP_SOAPCPP2}  "-v"   OUTPUT_VARIABLE GSOAP_STRING_VERSION ERROR_VARIABLE GSOAP_STRING_VERSION )
+  execute_process(COMMAND ${GSOAP_SOAPCPP2}  "-v" TIMEOUT 1 OUTPUT_VARIABLE GSOAP_STRING_VERSION ERROR_VARIABLE GSOAP_STRING_VERSION )
   string(REGEX MATCH "[0-9]*\\.[0-9]*\\.[0-9]*" GSOAP_VERSION ${GSOAP_STRING_VERSION})
 endif()
 
 message(STATUS " - GSOAP VERSION : ${GSOAP_VERSION}")
 if( "${GSOAP_VERSION}"  VERSION_LESS "2.7.6")
 	set(GSOAP_276_COMPAT_FLAGS "")
-elseif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14") 
-	set(GSOAP_276_COMPAT_FLAGS "-z")	
-else ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14") 
-	set(GSOAP_276_COMPAT_FLAGS "-z1 -z2")	
+elseif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14")
+	set(GSOAP_276_COMPAT_FLAGS "-z")
+else ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.14")
+	set(GSOAP_276_COMPAT_FLAGS "-z1 -z2")
 endif ( "${GSOAP_VERSION}"  VERSION_LESS "2.7.6")
 
 
 
 # -----------------------------------------------------
-# handle the QUIETLY and REQUIRED arguments and set GSOAP_FOUND to TRUE if 
+# handle the QUIETLY and REQUIRED arguments and set GSOAP_FOUND to TRUE if
 # all listed variables are TRUE
 # -----------------------------------------------------
 include(FindPackageHandleStandardArgs)
-find_package_handle_standard_args(gsoap DEFAULT_MSG GSOAP_LIBRARIES 
+find_package_handle_standard_args(gsoap DEFAULT_MSG GSOAP_LIBRARIES
     GSOAP_INCLUDE_DIR GSOAP_WSDL2H GSOAP_SOAPCPP2)
 mark_as_advanced(GSOAP_INCLUDE_DIR GSOAP_LIBRARIES GSOAP_WSDL2H GSOAP_SOAPCPP2)

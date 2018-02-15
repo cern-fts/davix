e193c9c66a08b4f58e189fad688ca0e25d55e65e
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-02-15 11:33:34 +0100

    CI: Install missing dependencies

--- packaging/make-docs.sh
+++ packaging/make-docs.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
+
+#-------------------------------------------------------------------------------
+# Make SRPM to get a list of build dependencies, this is ugly, fix.
+#-------------------------------------------------------------------------------
 git submodule update --init --recursive
+./packaging/make-srpm.sh
+sudo yum-builddep -y build/SRPMS/*
 
 #-------------------------------------------------------------------------------
 # Generate a docs folder - run this from the root of the git repository.

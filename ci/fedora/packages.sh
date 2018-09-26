c6f6d2f98c3be8020d9fef53f995d2099c2bad89
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-09-26 15:00:16 +0200

    CI: Try to fix fedora 28 build

--- ci/fedora/packages.sh
+++ ci/fedora/packages.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-yum install -y cmake git rpm-build yum-utils
+yum install -y cmake git rpm-build yum-utils make
 
 if which dnf; then
-  dnf install dnf-plugins-core
+  dnf install -y dnf-plugins-core
 fi

542b6491d2b6393c4df893c46d3ef62a8cd8a081
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-09-26 14:33:18 +0200

    CI: Install build dependencies for debian

--- ci/debian/packages.sh
+++ ci/debian/packages.sh
@@ -2,3 +2,4 @@
 
 apt-get update
 apt-get install -y git g++ cmake pbuilder devscripts equivs
+mk-build-deps --install --remove "packaging/debian/control" --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"

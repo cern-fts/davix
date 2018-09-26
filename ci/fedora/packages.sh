bb7447b1a4fdb959cd65c559507028949aefb9fd
Georgios Bitzes <georgios.bitzes@cern.ch>
2018-09-26 15:05:58 +0200

    CI: Add one more missing package dependency

--- ci/fedora/packages.sh
+++ ci/fedora/packages.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-yum install -y cmake git rpm-build yum-utils make
+yum install -y cmake git rpm-build yum-utils make which
 
 if which dnf; then
   dnf install -y dnf-plugins-core

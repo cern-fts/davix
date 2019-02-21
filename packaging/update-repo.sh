b869eda4d6ec8f972da19011f126d0027051fe02
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-02-21 11:36:03 +0100

    Attempt to make RPM generation more robust for dmc-repo

---
+++ packaging/update-repo.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+#-------------------------------------------------------------------------------
+# This script tries to improve robustness when regenerating a repo
+# which lives on EOS.
+#-------------------------------------------------------------------------------
+
+if [ -z "$1" ]; then
+  echo "syntax: $0 <path to repo>"
+  exit 1
+fi
+
+REPO="$1"
+createrepo -v "${REPO}" --update --workers 1
+
+# Force to access the file, or the size will be right, but the file empty
+cat "${REPO}/repodata/repomd.xml" > /dev/null
+# Stat the repomd.xml
+REPOSIZE=$(stat --printf "%s" "${REPO}/repodata/repomd.xml")
+# If 0, the bug is still there, so move ourselves from .repodata to the final destination
+if [[ $REPOSIZE == 0 ]]; then
+    echo "Trigger workaround for EOS"
+    mv "${REPO}/.repodata/repomd.xml" "${REPO}/repodata/repomd.xml"
+fi

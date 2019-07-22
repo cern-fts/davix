cern-fts/davix

commit dd3e1a4fff4f939d8634faf3ca9c75afbb7b4cc9

Move most remaining interface methods to BackendRequest

src/backend/BackendRequest.cpp
@@ -24,6 +24,8 @@
 #include <utils/davix_azure_utils.hpp>
 #include <utils/davix_gcloud_utils.hpp>
 #include <utils/davix_logger_internal.hpp>
+#include <string_utils/stringutils.hpp>
+#include <fileops/fileutils.hpp>
 #include <string>
 
 namespace Davix {
@@ -45,7 +47,10 @@ BackendRequest::BackendRequest(Context &c, const Uri &uri)
     _content_body(),
     _fd_content(-1),
     _content_provider(),
-    _internal_status(RequestStatus::kNotStarted) {}
+    _internal_status(RequestStatus::kNotStarted),
+    _ans_size(-1),
+    _early_termination(false),
+    _early_termination_error(NULL) {}
 
 
 //------------------------------------------------------------------------------
@@ -145,6 +150,29 @@ bool BackendRequest::checkTimeout(DavixError **err){
   }
 
   return false;
+}
+
+//------------------------------------------------------------------------------
+// Get answer size from headers.
+//------------------------------------------------------------------------------
+dav_ssize_t BackendRequest::getAnswerSizeFromHeaders() const {
+  std::string str_file_size="";
+
+  long size=-1;
+  if(getAnswerHeader(ans_header_content_length, str_file_size)) {
+    StrUtil::trim(str_file_size);
+    try{
+      size = toType<long, std::string>()(str_file_size);
+    } catch(...){
+      size = -1;
+    }
+  }
+
+  if(size == -1){
+    DAVIX_SLOG(DAVIX_LOG_TRACE, DAVIX_LOG_HTTP, "Bad server answer: {} Invalid, impossible to determine answer size", ans_header_content_length);
+  }
+
+  return static_cast<dav_ssize_t>(size);
 }
 
 //------------------------------------------------------------------------------
@@ -292,6 +320,40 @@ std::vector<char>& BackendRequest::getAnswerContentVec() {
 //------------------------------------------------------------------------------
 void BackendRequest::clearAnswerContent() {
   _vec.clear();
+}
+
+//------------------------------------------------------------------------------
+// Parse "Last-Modified" response header, return as time_t.
+//------------------------------------------------------------------------------
+time_t BackendRequest::getLastModified() const {
+  time_t t=0;
+
+  std::string str_lastmodified;
+  if(getAnswerHeader("Last-Modified", str_lastmodified)) {
+    StrUtil::trim(str_lastmodified);
+    try{
+      t = S3::s3TimeConverter(str_lastmodified);
+    } catch(...){
+      str_lastmodified.clear();
+    }
+  }
+
+  if(str_lastmodified.empty()){
+    DAVIX_SLOG(DAVIX_LOG_TRACE, DAVIX_LOG_HTTP, "Bad server answer: {} Invalid, impossible to determine last modified time");
+  }
+
+  return t;
+}
+
+//------------------------------------------------------------------------------
+// Get answer size
+//------------------------------------------------------------------------------
+dav_ssize_t BackendRequest::getAnswerSize() const {
+  if(_ans_size < 0) {
+    return getAnswerSizeFromHeaders();
+  }
+
+  return _ans_size;
 }
 
 

src/backend/BackendRequest.hpp
@@ -79,6 +79,31 @@ public:
   virtual dav_ssize_t readBlock(char* buffer, dav_size_t max_size, DavixError** err) = 0;
 
   //----------------------------------------------------------------------------
+  // Get a specific response header
+  //----------------------------------------------------------------------------
+  virtual bool getAnswerHeader(const std::string &header_name, std::string &value) const = 0;
+
+  //----------------------------------------------------------------------------
+  // Get all response headers
+  //----------------------------------------------------------------------------
+  virtual size_t getAnswerHeaders(std::vector<std::pair<std::string, std::string > > & vec_headers) const = 0;
+
+  //----------------------------------------------------------------------------
+  // Start request.
+  //----------------------------------------------------------------------------
+  virtual int beginRequest(DavixError** err) = 0;
+
+  //----------------------------------------------------------------------------
+  // Finish an already started request.
+  //----------------------------------------------------------------------------
+  virtual int endRequest(DavixError** err) = 0;
+
+  //----------------------------------------------------------------------------
+  // Get response status.
+  //----------------------------------------------------------------------------
+  virtual int getRequestCode() = 0;
+
+  //----------------------------------------------------------------------------
   // Helper read members - implemented in terms of readBlock, and an internal
   // buffer.
   //----------------------------------------------------------------------------
@@ -166,6 +191,16 @@ public:
   //----------------------------------------------------------------------------
   void clearAnswerContent();
 
+  //----------------------------------------------------------------------------
+  // Parse "Last-Modified" response header, return as time_t.
+  //----------------------------------------------------------------------------
+  time_t getLastModified() const;
+
+  //----------------------------------------------------------------------------
+  // Get answer size
+  //----------------------------------------------------------------------------
+  dav_ssize_t getAnswerSize() const;
+
 protected:
   //----------------------------------------------------------------------------
   // Configure request for S3.
@@ -191,6 +226,11 @@ protected:
   // Check if deadline has already passed
   //----------------------------------------------------------------------------
   bool checkTimeout(DavixError **err);
+
+  //----------------------------------------------------------------------------
+  // Get answer size from headers.
+  //----------------------------------------------------------------------------
+  dav_ssize_t getAnswerSizeFromHeaders() const;
 
   //----------------------------------------------------------------------------
   // Member variables common to all implementations.
@@ -219,10 +259,21 @@ protected:
   RequestStatus _internal_status;
 
   //----------------------------------------------------------------------------
+  // Answer length.
+  //----------------------------------------------------------------------------
+  dav_ssize_t _ans_size;
+
+  //----------------------------------------------------------------------------
   // Answer buffers.
   //----------------------------------------------------------------------------
   std::vector<char> _vec;
   std::vector<char> _vec_line;
+
+  //----------------------------------------------------------------------------
+  // Early termination flag and status.
+  //----------------------------------------------------------------------------
+  bool _early_termination;
+  DavixError* _early_termination_error;
 
 };
 

src/neon/neonrequest.cpp
@@ -85,10 +85,7 @@ void configureRequestParamsProto(const Uri &uri, RequestParams &params){
             params.setProtocol(RequestProtocol::Gcloud);
         }
     }
-
 }
-
-
 
 void neon_generic_error_mapper(int ne_status, StatusCode::Code & code, std::string & str){
     switch(ne_status){
@@ -164,13 +161,10 @@ NEONRequest::NEONRequest(HttpRequest & h, Context& context, const Uri & uri_req)
     _redirects(0),
     _total_read_size(0),
     _last_read(-1),
-    _ans_size(-1),
     _h(h),
     req_started(false),
     req_running(false),
-    _accepted_202_retries(0),
-    _early_termination(0),
-    _early_termination_error(NULL) {
+    _accepted_202_retries(0) {
 }
 
 
@@ -755,6 +749,9 @@ int NEONRequest::endRequest(DavixError** err){
     return 0;
 }
 
+//------------------------------------------------------------------------------
+// Get response status.
+//------------------------------------------------------------------------------
 int NEONRequest::getRequestCode(){
     if(_early_termination) {
         if(!_early_termination_error) return 200;
@@ -763,49 +760,7 @@ int NEONRequest::getRequestCode(){
     return ne_get_status(_req)->code;
 }
 
-dav_ssize_t NEONRequest::getAnswerSizeFromHeaders() const{
-    std::string str_file_size="";
-    long size=-1;
-    if( getAnswerHeader(ans_header_content_length, str_file_size)){
-        StrUtil::trim(str_file_size);
-        try{
-            size = toType<long, std::string>()(str_file_size);
-        }catch(...){
-            size = -1;
-        }
-    }
-    if( size == -1){
-       DAVIX_SLOG(DAVIX_LOG_TRACE, DAVIX_LOG_HTTP, "Bad server answer: {} Invalid, impossible to determine answer size", ans_header_content_length);
-    }
-    return static_cast<dav_ssize_t>(size);
-}
-
-dav_ssize_t NEONRequest::getAnswerSize() const{
-    if(_ans_size < 0)
-        _ans_size = getAnswerSizeFromHeaders();
-    return _ans_size;
-}
-
-
-time_t NEONRequest::getLastModified() const{
-    time_t t=0;
-    std::string str_lastmodified;
-    if( getAnswerHeader("Last-Modified", str_lastmodified)){
-        StrUtil::trim(str_lastmodified);
-        try{
-            t = S3::s3TimeConverter(str_lastmodified);
-        }catch(...){
-            str_lastmodified.clear();
-        }
-    }
-    if( str_lastmodified.empty()){
-       DAVIX_SLOG(DAVIX_LOG_TRACE, DAVIX_LOG_HTTP, "Bad server answer: {} Invalid, impossible to determine last modified time");
-    }
-    return t;
-}
-
-
-bool NEONRequest::getAnswerHeader(const std::string &header_name, std::string &value) const{
+bool NEONRequest::getAnswerHeader(const std::string &header_name, std::string &value) const {
     if(_req){
         const char* answer_content = ne_get_response_header(_req, header_name.c_str());
         if(answer_content){
@@ -816,8 +771,7 @@ bool NEONRequest::getAnswerHeader(const std::string &header_name, std::string &v
     return false;
 }
 
-
-size_t NEONRequest::getAnswerHeaders( HeaderVec & vec_headers) const{
+size_t NEONRequest::getAnswerHeaders( HeaderVec & vec_headers) const {
     if(_req){
         void * handle = NULL;
         const char* name=NULL, *value=NULL;

src/neon/neonrequest.hpp
@@ -68,28 +68,27 @@ public:
     //--------------------------------------------------------------------------
     // Start request.
     //--------------------------------------------------------------------------
-    int beginRequest(DavixError** err);
+    virtual int beginRequest(DavixError** err);
 
     //--------------------------------------------------------------------------
     // Finish an already started request.
     //--------------------------------------------------------------------------
-    int endRequest(DavixError** err);
+    virtual int endRequest(DavixError** err);
 
-    /**
-     * get content length
-     **/
-    dav_ssize_t getAnswerSize() const;
+    //--------------------------------------------------------------------------
+    // Get response status.
+    //--------------------------------------------------------------------------
+    virtual int getRequestCode();
 
-    /**
-     * get last modified
-     **/
-    time_t getLastModified() const;
+    //--------------------------------------------------------------------------
+    // Get a specific response header
+    //--------------------------------------------------------------------------
+    virtual bool getAnswerHeader(const std::string &header_name, std::string &value) const;
 
-    int getRequestCode();
-
-    bool getAnswerHeader(const std::string &header_name, std::string &value) const;
-
-    size_t getAnswerHeaders( std::vector<std::pair<std::string, std::string > > & vec_headers) const;
+    //--------------------------------------------------------------------------
+    // Get all response headers
+    //--------------------------------------------------------------------------
+    virtual size_t getAnswerHeaders(std::vector<std::pair<std::string, std::string > > & vec_headers) const;
 
 private:
 
@@ -105,21 +104,14 @@ private:
     // read info
     dav_ssize_t _total_read_size, _last_read;
 
-    // answer length
-    mutable dav_ssize_t _ans_size;
 
     HttpRequest & _h;
     bool req_started, req_running;
 
     int _accepted_202_retries;
 
-    bool _early_termination;
-    DavixError* _early_termination_error;
-
     ////////////////////////////////////////////
     // Private Members
-    dav_ssize_t getAnswerSizeFromHeaders() const;
-
     int startRequest(DavixError** err);
 
     int processRedirection(int neonCode, DavixError** err); // analyze and process redirection if needed

commit 78de96a33f5f8c8513c1e108f01791683f2304a2

Fix answer-size related bug introduced in dd3e1a4

src/backend/BackendRequest.cpp
@@ -350,7 +350,7 @@ time_t BackendRequest::getLastModified() const {
 //------------------------------------------------------------------------------
 dav_ssize_t BackendRequest::getAnswerSize() const {
   if(_ans_size < 0) {
-    return getAnswerSizeFromHeaders();
+    _ans_size = getAnswerSizeFromHeaders();
   }
 
   return _ans_size;

src/backend/BackendRequest.hpp
@@ -266,7 +266,7 @@ protected:
   //----------------------------------------------------------------------------
   // Answer length.
   //----------------------------------------------------------------------------
-  dav_ssize_t _ans_size;
+  mutable dav_ssize_t _ans_size;
 
   //----------------------------------------------------------------------------
   // Answer buffers.

test/functional/davix-tester.cpp
@@ -235,6 +235,7 @@ void statfileFromSignedURI(const RequestParams &params, const Uri uri) {
   StatInfo info;
   file.statInfo(&params2, info);
   std::cout << string_from_mode(info.mode) << std::endl;
+  std::cout << info.size << std::endl;
 
   ASSERT(! S_ISDIR(info.mode), "not a file");
 }
@@ -247,6 +248,7 @@ void statfile(const RequestParams &params, const Uri uri) {
     StatInfo info;
     file.statInfo(&params, info);
     std::cout << string_from_mode(info.mode) << std::endl;
+    std::cout << info.size << std::endl;
 
     ASSERT(! S_ISDIR(info.mode), "not a file");
 
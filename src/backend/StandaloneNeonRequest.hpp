539ac92fd53c539e85a179122e9035711fc6d8a0
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-10-04 11:01:33 +0200

    Implement more functionality in StandaloneNeonRequest: a no-magic HTTP request

--- src/backend/StandaloneNeonRequest.hpp
+++ src/backend/StandaloneNeonRequest.hpp
@@ -23,6 +23,10 @@
 #define DAVIX_BACKEND_STANDALONE_NEON_REQUEST_HPP
 
 #include "BoundHooks.hpp"
+#include <davix_internal.hpp>
+#include <utils/davix_uri.hpp>
+#include <ne_request.h>
+#include <params/davixrequestparams.hpp>
 #include <memory>
 
 namespace Davix {
@@ -33,11 +37,24 @@ class RequestParams;
 class NeonSessionWrapper;
 
 //------------------------------------------------------------------------------
+// Describe current request state.
+//------------------------------------------------------------------------------
+enum class RequestState {
+  kNotStarted = 0,      // request object has been constructed,
+                        // but startRequest has not been called yet
+
+  kStarted,             // startRequest has been called
+
+  kFinished             // request is done, session has been released
+};
+
+
+//------------------------------------------------------------------------------
 // Represent a single, standalone HTTP request with libneon implementation.
 //
 // No magic here. No automatic redirections, no cached redirections, no
-// compatibility hacks, no automatic retries, nothing. Just a single
-// HTTP request / response.
+// compatibility hacks, no automatic retries, no automatic URL signing for S3
+// or Azure -- NOTHING. Just a single HTTP request / response.
 //
 // The magic is to be added by higher-level classes building upon this one.
 //------------------------------------------------------------------------------
@@ -58,18 +75,70 @@ public:
   //
   // param: Request parameters, authentication, etc.
   //----------------------------------------------------------------------------
-  StandaloneNeonRequest(NEONSessionFactory *sessionFactory, bool reuseSession,
-    const BoundHooks &boundHooks, const Uri &uri, const RequestParams &param);
+  StandaloneNeonRequest(NEONSessionFactory &sessionFactory, bool reuseSession,
+    const BoundHooks &boundHooks, const Uri &uri, const std::string &verb, const RequestParams &params,
+    const std::vector<HeaderLine> &headers, int requestFlag, ContentProvider *contentProvider,
+    Chrono::TimePoint deadline);
 
   //----------------------------------------------------------------------------
   // Destructor
   //----------------------------------------------------------------------------
   virtual ~StandaloneNeonRequest();
 
+  //----------------------------------------------------------------------------
+  // Get a specific response header
+  //----------------------------------------------------------------------------
+  bool getAnswerHeader(const std::string &header_name, std::string &value) const;
+
+  //----------------------------------------------------------------------------
+  // Get all response headers
+  //----------------------------------------------------------------------------
+  size_t getAnswerHeaders(std::vector<std::pair<std::string, std::string > > & vec_headers) const;
+
+  //----------------------------------------------------------------------------
+  // Start request - calling this more than once will not have any effect.
+  //----------------------------------------------------------------------------
+  void startRequest(DavixError **err);
+
+  //----------------------------------------------------------------------------
+  // Finish an already started request.
+  //----------------------------------------------------------------------------
+  void endRequest(DavixError** err);
+
+  //----------------------------------------------------------------------------
+  // Major read function - read a block of max_size bytes (at max) into buffer.
+  //----------------------------------------------------------------------------
+  dav_ssize_t readBlock(char* buffer, dav_size_t max_size, DavixError** err);
 
 private:
+  NEONSessionFactory &_session_factory;
+  bool _reuse_session;
   BoundHooks _bound_hooks;
-  // std::unique_ptr<NeonSessionWrapper> _session;
+  Uri _uri;
+  std::string _verb;
+  RequestParams _params;
+  RequestState _state;
+  std::vector<HeaderLine> _headers;
+  int _req_flag;
+  ContentProvider *_content_provider;
+  Chrono::TimePoint _deadline;
+
+  friend class NeonSessionWrapper;
+  std::unique_ptr<NeonSessionWrapper> _session;
+  ne_request* _neon_req;
+  dav_ssize_t _total_read_size;
+  dav_ssize_t _last_read;
+
+  //----------------------------------------------------------------------------
+  // Check if timeout has passed
+  //----------------------------------------------------------------------------
+  bool checkTimeout(DavixError **err);
+
+  //----------------------------------------------------------------------------
+  // Mark request as completed, release any resources
+  //----------------------------------------------------------------------------
+  void markCompleted();
+
 
 };
 

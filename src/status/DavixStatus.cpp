fc97ac22d95fb5e142beb2aa42105678288038bd
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-10-08 13:49:29 +0200

    Implement Davix::Status as an alternative to DavixError
    DMC-1183

---
+++ src/status/DavixStatus.cpp
@@ -0,0 +1,114 @@
+/*
+ * This File is part of Davix, The IO library for HTTP based protocols
+ * Copyright (C) CERN 2019
+ * Author: Georgios Bitzes <georgios.bitzes@cern.ch>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+*/
+
+#include <status/DavixStatus.hpp>
+
+namespace Davix {
+
+//----------------------------------------------------------------------------
+// Empty constructor - OK status, with empty error message, and empty scope
+//----------------------------------------------------------------------------
+Status::Status() {
+  d_ptr = NULL;
+}
+
+//----------------------------------------------------------------------------
+// Constructor - specify scope, errno, and error message
+//----------------------------------------------------------------------------
+Status::Status(const std::string &scope, StatusCode::Code errcode, const std::string& errmsg) {
+  d_ptr = new DavixErrorInternal(scope, errcode, errmsg);
+}
+
+//------------------------------------------------------------------------------
+// Destructor
+//------------------------------------------------------------------------------
+Status::~Status() {
+  if(d_ptr) {
+    delete d_ptr;
+    d_ptr = NULL;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Copy constructor
+//------------------------------------------------------------------------------
+Status::Status(const Status &other) {
+  *this = other;
+}
+
+//------------------------------------------------------------------------------
+// Copy assignment operator
+//------------------------------------------------------------------------------
+Status& Status::operator=(const Status &other) {
+  if(other.d_ptr) {
+    d_ptr = new DavixErrorInternal(*other.d_ptr);
+  }
+  else {
+    d_ptr = NULL;
+  }
+
+  return *this;
+}
+
+//------------------------------------------------------------------------------
+// Check if status indicates any error
+//------------------------------------------------------------------------------
+bool Status::ok() const {
+  return getCode() == StatusCode::OK;
+}
+
+//------------------------------------------------------------------------------
+// Get status code
+//------------------------------------------------------------------------------
+StatusCode::Code Status::getCode() const {
+  if(!d_ptr) {
+    return StatusCode::OK;
+  }
+  else {
+    return d_ptr->_code;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Get error message
+//------------------------------------------------------------------------------
+std::string Status::getErrorMessage() const {
+  if(!d_ptr) {
+    return std::string();
+  }
+  else {
+    return d_ptr->_errMsg;
+  }
+}
+
+//------------------------------------------------------------------------------
+// Get scope
+//------------------------------------------------------------------------------
+std::string Status::getScope() const {
+  if(!d_ptr) {
+    return std::string();
+  }
+  else {
+    return d_ptr->_scope;
+  }
+}
+
+}

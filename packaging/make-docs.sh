db34735cbd1dabedcb3056a87747b859746cccd1
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-11-05 16:24:06 +0100

    Fix location of sphinx directory

--- packaging/make-docs.sh
+++ packaging/make-docs.sh
@@ -12,8 +12,10 @@ dnf builddep -y build/SRPMS/*
 # Generate a docs folder - run this from the root of the git repository.
 #-------------------------------------------------------------------------------
 rm -rf build
+SPHINX_DIR="${PWD}/sphinx"
+
 mkdir build && cd build
-sphinx-build-3 -q -b html "${PWD}/sphinx" "${PWD}/build/html/"
+sphinx-build-3 -q -b html "${SPHINX_DIR}" "${PWD}/build/html/"
 
 cmake -DENABLE_HTML_DOCS=TRUE ..
 make doc

04514410358fb6089e27bd3db42e1771a8e7e861
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-11-06 17:40:21 +0100

    Add autoconf, automake to CI packages

--- ci/debian/packages.sh
+++ ci/debian/packages.sh
@@ -2,5 +2,5 @@
 set -e
 
 apt-get update
-apt-get install -y git g++ cmake pbuilder devscripts equivs
+apt-get install -y git g++ cmake pbuilder devscripts equivs autoconf automake
 mk-build-deps --install --remove "packaging/debian/control" --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"

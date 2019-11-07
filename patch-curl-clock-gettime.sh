8d87d844792837b110f9d8c1b8fdd686d0378589
Georgios Bitzes <georgios.bitzes@cern.ch>
2019-11-07 13:54:20 +0100

    Only patch out clock_gettime support on SLC6

--- patch-curl-clock-gettime.sh
+++ patch-curl-clock-gettime.sh
@@ -7,11 +7,19 @@
 #
 # This script patches out libcurl support for clock_gettime entirely when building on SLC6.
 
-set -ex
+set -x
 
-#if [ -f config.status ]; then
+if [ -f lib/curl_config.h ]; then
+  echo '#include<time.h>
+        int main() { struct timespec ts; clock_gettime(CLOCK_MONOTONIC, &ts); return 0; }' &> test.c
+  gcc test.c -o /dev/null
+
+  if [ $? -ne 0 ]; then
     grep "HAVE_CLOCK_GETTIME" lib/curl_config.h
-    sed -i 's|#define HAVE_CLOCK_GETTIME_MONOTONIC 1|// #define HAVE_CLOCK_GETTIME_MONOTONIC 1|g' lib/curl_config.h
+    sed -i 's|^#define HAVE_CLOCK_GETTIME_MONOTONIC 1|// #define HAVE_CLOCK_GETTIME_MONOTONIC 1|g' lib/curl_config.h
     grep "HAVE_CLOCK_GETTIME" lib/curl_config.h
-#fi
+  fi
+
+  rm test.c
+fi
 

738f89ce4b86eac9ecd96ad61edf7c05289d9ec0
Georgios Bitzes <georgios.bitzes@cern.ch>
2020-03-06 13:16:48 +0100

    Disable all weird libcurl protocols and features when bundling

--- cmake/modules/buildCurl.cmake
+++ cmake/modules/buildCurl.cmake
@@ -6,11 +6,46 @@ macro(buildCurl)
     set(SECURE_TRANSPORT_FLAGS "--with-secure-transport")
   endif()
 
+  string(JOIN " "
+    DISABLE_CURL_FEATURES
+    "--disable-ftp"
+    "--disable-ldap"
+    "--disable-ldaps"
+    "--disable-rtsp"
+    "--disable-dict"
+    "--disable-telnet"
+    "--disable-tftp"
+    "--disable-pop3"
+    "--disable-imap"
+    "--disable-smb"
+    "--disable-smtp"
+    "--disable-gopher"
+    "--disable-sspi"
+    "--disable-ntlm-wb"
+    "--disable-netrc"
+
+    "--without-winssl"
+    "--without-schannel"
+    "--without-darwinssl"
+    "--without-amissl"
+    "--without-gnutls"
+    "--without-mbedtls"
+    "--without-wolfssl"
+    "--without-mesalink"
+    "--without-nss"
+    "--without-libpsl"
+    "--without-libmetalink"
+    "--without-librtmp"
+    "--without-winidn"
+
+    "--without-libidn2"
+  )
+
   ExternalProject_Add(BuildCurlBundled
       SOURCE_DIR "${CMAKE_SOURCE_DIR}/deps/curl"
       PREFIX "${CMAKE_BINARY_DIR}/curl"
       BUILD_IN_SOURCE 1
-      CONFIGURE_COMMAND bash -c "autoreconf -i && ./configure  --prefix=${CMAKE_CURRENT_BINARY_DIR}/curl --disable-ftp --disable-ldap --disable-ldaps --disable-telnet --disable-tftp --disable-pop3 --disable-imap --disable-smb --disable-smtp --disable-gopher --without-libidn2 ${SECURE_TRANSPORT_FLAGS} && ${CMAKE_SOURCE_DIR}/patch-curl-clock-gettime.sh"
+      CONFIGURE_COMMAND bash -c "autoreconf -i && ./configure  --prefix=${CMAKE_CURRENT_BINARY_DIR}/curl ${DISABLE_CURL_FEATURES} ${SECURE_TRANSPORT_FLAGS} && ${CMAKE_SOURCE_DIR}/patch-curl-clock-gettime.sh"
       BUILD_COMMAND ${MAKE}
   )
 

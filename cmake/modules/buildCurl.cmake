27f448b20fa95e499b6731bb752ac458d7e2cc40
Georgios Bitzes <georgios.bitzes@cern.ch>
2020-03-06 14:28:26 +0100

    Fix bundled curl's installation paths

--- cmake/modules/buildCurl.cmake
+++ cmake/modules/buildCurl.cmake
@@ -10,13 +10,13 @@ macro(buildCurl)
       SOURCE_DIR "${CMAKE_SOURCE_DIR}/deps/curl"
       BINARY_DIR "${CMAKE_BINARY_DIR}/deps/curl"
       PREFIX "${CMAKE_BINARY_DIR}/deps/curl"
-      CONFIGURE_COMMAND bash -c "${CMAKE_COMMAND} -DHTTP_ONLY=ON -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_USE_LIBSSH2=OFF ${CMAKE_SOURCE_DIR}/deps/curl && ${CMAKE_SOURCE_DIR}/patch-curl-clock-gettime.sh"
+      CONFIGURE_COMMAND bash -c "${CMAKE_COMMAND} -DCMAKE_INSTALL_PREFIX=/ -DCMAKE_INSTALL_LIBDIR=lib -DHTTP_ONLY=ON -DBUILD_CURL_EXE=OFF -DBUILD_TESTING=OFF -DBUILD_SHARED_LIBS=OFF -DCMAKE_POSITION_INDEPENDENT_CODE=ON -DCMAKE_USE_LIBSSH2=OFF ${SECURE_TRANSPORT_FLAGS} ${CMAKE_SOURCE_DIR}/deps/curl && ${CMAKE_SOURCE_DIR}/patch-curl-clock-gettime.sh"
       BUILD_COMMAND make
       INSTALL_COMMAND make DESTDIR=${CMAKE_BINARY_DIR}/deps/curl-install install
   )
 
   add_library(libcurl STATIC IMPORTED)
-  set_property(TARGET libcurl PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/deps/curl-install/usr/local/lib/libcurl.a)
+  set_property(TARGET libcurl PROPERTY IMPORTED_LOCATION ${CMAKE_CURRENT_BINARY_DIR}/deps/curl-install/usr/lib/libcurl.a)
   add_dependencies(libcurl BuildCurlBundled)
 
   # Replace with the following when possible:

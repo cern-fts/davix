6b3d898d7b2b1739059da7148d76090093fede44
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2024-12-31 16:09:09 -0800

    DMC-1429: Fix lintian changelog warnings

--- packaging/make-deb.sh
+++ packaging/make-deb.sh
@@ -47,7 +47,7 @@ if [[ ! -z "$MINIPATCH" ]]; then
   echo ""                                                          >> patched-changelog
   echo "  * CI build, update to version ${VERSION_FULL}"           >> patched-changelog
   echo ""                                                          >> patched-changelog
-  echo " -- CI Robot <davix.tests at cern.ch>  ${CURRENT_DATE}"    >> patched-changelog
+  echo " -- CI Robot <davix.tests@cern.ch>  ${CURRENT_DATE}"       >> patched-changelog
   echo ""                                                          >> patched-changelog
 
   cat patched-changelog changelog > tmp

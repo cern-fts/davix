d8d85f87902327a1f56235ecbb76fb8a4d3f1c73
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2025-01-10 11:51:17 -0800

    DMC-1430: Ubuntu Jammy build

--- ci/debian/packages.sh
+++ ci/debian/packages.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-apt-get update
-apt-get install -y git g++ cmake pbuilder devscripts equivs autoconf automake
-mk-build-deps --install --remove "packaging/debian/control" --tool "apt-get -o Debug::pkgProblemResolver=yes --no-install-recommends -y"
+apt update
+apt install -y git g++ cmake pbuilder devscripts equivs autoconf automake
+mk-build-deps --install --remove "packaging/debian/control" --tool "apt -o Debug::pkgProblemResolver=yes --no-install-recommends -y"

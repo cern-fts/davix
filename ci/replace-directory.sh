9bf660491c699a0559471afaab42c5f1de97eb72
Mihai Patrascoiu <mihai.patrascoiu@cern.ch>
2025-01-21 14:15:45 +0100

    CI: Always use a unique temporary target location in the documentation directory replacement script
    Currently, the docs publishing jobs fails intermittently with the following error:
    "rm: cannot remove <target-old>: File exists"
    
    It is not clearly understood what causes the intermittent EOS FUSE failures, but some level of write+delete concurrency is suspected. By making the temporary target location unique, the race condition should no longer happen

--- ci/replace-directory.sh
+++ ci/replace-directory.sh
@@ -3,11 +3,23 @@ set -ex
 
 STAGING_AREA="$1"
 TARGET="$2"
-OLD_CONTENTS="${TARGET}-old"
+SNAPSHOT="$3"
 
-test ! -d "$TARGET" || mv "$TARGET" "$OLD_CONTENTS"
+if [[ -z "${SNAPSHOT}" ]]; then
+  SNAPSHOT=$(date +%s)
+fi
 
-sleep 30
-mv "$STAGING_AREA" "$TARGET"
-test ! -d "$OLD_CONTENTS" || rm -rf "$OLD_CONTENTS"
+TARGET_OLD="${TARGET}-old-${SNAPSHOT}"
 
+if [[ -d "${TARGET}" ]]; then
+  mv -v "${TARGET}" "${TARGET_OLD}"
+fi
+
+# Allow EOS FUSE grace time
+sleep 10
+
+mv -v "${STAGING_AREA}" "${TARGET}"
+
+if [[ -d "${TARGET_OLD}" ]]; then
+  rm -rf "${TARGET_OLD}/"
+fi
